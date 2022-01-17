1b92d98d60a46736c1bceb82724f57fb98f23c19
Antti <anttituomola8@gmail.com>
2022-01-17 21:30:06 +0200

    decide winner function

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
 let dataFeed = []
 
+// Get the data from the API
 fetch("https://blooming-retreat-52483.herokuapp.com/https://bad-api-assignment.reaktor.com/rps/history/")
     .then(res => res.json())
     .then(data => {
         dataFeed = data
-        checkData(data)
+        groupData(dataFeed)
+        decideWinner(dataFeed)
     })
-    
-function checkData(data) {
-    const dataFeed = data.data
-    groupData(dataFeed)
-}
 
-function groupData() {
-    const groupedByPlayer = Object.entries(dataFeed.data).reduce((acc, game) => {
-        if (!acc[game[1].playerA.name]) {
-            acc[game[1].playerA.name] = []
+// Group the games by player
+function groupData(data) {
+    const groupedByPlayer = Object.entries(data.data).reduce((player, game) => {
+        // Go through players A
+        if (!player[game[1].playerA.name]) {
+            player[game[1].playerA.name] = []
         }
     
-        acc[game[1].playerA.name].push(game)
+        player[game[1].playerA.name].push(game)
+
+        //Go through players B
+        if (!player[game[1].playerB.name]) {
+            player[game[1].playerB.name] = []
+        }
     
-        return acc
+        player[game[1].playerB.name].push(game)
+    
+        return player
     }, {})
-    
-    printPlayers(groupedByPlayer)
+}
+
+function decideWinner(dataFeed) {
+    console.log(dataFeed.data)
+    let winnersAdded = {}
+    winnersAdded = Object.entries(dataFeed.data).map(game => {
+        console.log(game)
+        if (game[1].playerA.played === "SCISSORS" && game[1].playerB.played === "PAPER") {
+            game[1].winner = "playerA"
+        } else if (game[1].playerA.played === "PAPER" && game[1].playerB.played === "ROCK") {
+            game[1].winner = "playerA"
+        } else if (game[1].playerA.played === "ROCK" && game[1].playerB.played === "SCISSORS") {
+            game[1].winner = "playerA"
+        } else if (game[1].playerA.played === game[1].playerB.played) {
+            game[1].winner = ""
+        } else {
+            game[1].winner = "playerB"
+        }
+        return game
+    })
+    groupData(winnersAdded)
 }
 
 function printPlayers(groupedByPlayer) {
-    const names = Object.getOwnPropertyNames(groupedByPlayer)    
-    const namesInDiv = names.map(name => {
-        return `<div class="playerBox">
-        <h1>${name}</h1>
+    const renderPlayers = Object.entries(groupedByPlayer).map(player => {
+        console.log(player)
+        return `
+        <h1>${player[0]}</h1>
         <div class="winRatio"></div>
-        
-        </div>`
+        `
     })
 
     const playersDiv = document.getElementById("players")
-    playersDiv.innerHTML = namesInDiv.join("")
+    playersDiv.innerHTML = renderPlayers.join("")
 
 }
