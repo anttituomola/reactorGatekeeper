16a64cccd7eb9778a7fee197f8c1f8fb12cd8f68
Antti <anttituomola8@gmail.com>
2022-01-18 15:26:46 +0200

    initial styles

--- printPlayers.js
+++ printPlayers.js
@@ -2,10 +2,12 @@ export default function printPlayers(data) {
     console.log(data)
     const renderPlayers = Object.entries(data).map(player => {
         return `
-        <h1>${player[1][1][1][1][0]} (${player[1][1][1][0]})</h1>
-        <div class="winRatio">Win ratio: <b>${player[1][1][1][1].winRatio}</b></div>
-        <div class="playedGames">Games played: <b>${player[1][1][1][1][1].length}</b></div>
-        <div class"mostPlayedHand">Most played hand: <b>${player[1].mostCommondHand}</b></div>
+        <div class="aPlayer">
+            <h1>${player[1][1][1][1][0]} (${player[1][1][1][0]})</h1>
+            <div class="winRatio">Win ratio: <b>${player[1][1][1][1].winRatio}</b></div>
+            <div class="playedGames">Games played: <b>${player[1][1][1][1][1].length}</b></div>
+            <div class"mostPlayedHand">Most played hand: <b>${player[1].mostCommondHand}</b></div>
+        </div>
         `
     })
 
