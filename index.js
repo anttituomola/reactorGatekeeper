6cc9da44f705ba28077c0e6d6301ec02bd96f622
Antti <anttituomola8@gmail.com>
2022-01-18 11:55:39 +0200

    most common hand

--- index.js
+++ index.js
@@ -5,36 +5,35 @@ fetch("https://blooming-retreat-52483.herokuapp.com/https://bad-api-assignment.r
     .then(res => res.json())
     .then(data => {
         dataFeed = data
-        groupData(dataFeed)
         decideWinner(dataFeed)
     })
 
 // Group the games by player
 function groupData(data) {
-    const groupedByPlayer = Object.entries(data.data).reduce((player, game) => {
+    const groupedByPlayer = Object.entries(data).reduce((player, game) => {
         // Go through players A
-        if (!player[game[1].playerA.name]) {
-            player[game[1].playerA.name] = []
+        if (!player[game[1][1].playerA.name]) {
+            player[game[1][1].playerA.name] = []
         }
-    
-        player[game[1].playerA.name].push(game)
+
+        player[game[1][1].playerA.name].push(game)
 
         //Go through players B
-        if (!player[game[1].playerB.name]) {
-            player[game[1].playerB.name] = []
+        if (!player[game[1][1].playerB.name]) {
+            player[game[1][1].playerB.name] = []
         }
-    
-        player[game[1].playerB.name].push(game)
-    
+
+        player[game[1][1].playerB.name].push(game)
+
         return player
     }, {})
+    printPlayers(groupedByPlayer)
+    addHandsArray(groupedByPlayer)
 }
 
 function decideWinner(dataFeed) {
-    console.log(dataFeed.data)
     let winnersAdded = {}
     winnersAdded = Object.entries(dataFeed.data).map(game => {
-        console.log(game)
         if (game[1].playerA.played === "SCISSORS" && game[1].playerB.played === "PAPER") {
             game[1].winner = "playerA"
         } else if (game[1].playerA.played === "PAPER" && game[1].playerB.played === "ROCK") {
@@ -51,12 +50,45 @@ function decideWinner(dataFeed) {
     groupData(winnersAdded)
 }
 
+function addHandsArray(data) {
+    console.log(data)
+    const hands = Object.entries(data).map(player => {
+        let handsArray = []
+        player["handsArray"] = handsArray
+        player[1].map(game => {
+            if (player[0] === game[1][1].playerA.name) {
+                player.handsArray.push(game[1][1].playerA.played)
+            }
+
+            if (player[0] === game[1][1].playerB.name) {
+                player.handsArray.push(game[1][1].playerB.played)
+            }
+        })
+        return player
+    })
+    findMostCommonHand(hands)
+}
+
+function findMostCommonHand(hands) {
+    const mostCommondHand = Object.entries(hands).map(player => {
+        const hashmap = player[1].handsArray.reduce((acc, val) => {
+            acc[val] = (acc[val] || 0) + 1
+            return acc
+        }, {})
+         player.mostCommondHand =  Object.keys(hashmap).reduce((a, b) => hashmap[a] > hashmap[b] ? a : b)
+         return player
+    })
+    console.log(mostCommondHand)
+}
+
+
 function printPlayers(groupedByPlayer) {
     const renderPlayers = Object.entries(groupedByPlayer).map(player => {
-        console.log(player)
         return `
         <h1>${player[0]}</h1>
         <div class="winRatio"></div>
+        <div class="playedGames">Games played: ${player[1].length}</div>
+        <div class"mostPlayedHand">Most played hand: </div>
         `
     })
 
